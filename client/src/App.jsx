4a9e46f64b9ed819ac1cdf8c4a502925528ada6f
Thuan Nguyen <thuannv06022001@gmail.com>
2025-09-23 21:32:12 +0700

    update website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
+import RoomDetails from "./pages/RoomDetails";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,14 +13,13 @@ const App = () => {
       {!isOwnerPath && <Navbar />}
       <div className="min-h-[70vh]">
         <Routes>
-          <Route path='/' element={<Home />} >
-          </Route>
-          <Route path='/rooms' element={<AllRooms />} >
-          </Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/rooms" element={<AllRooms />}></Route>
+          <Route path="/rooms/:id" element={<RoomDetails />}></Route>
         </Routes>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 export default App;
