ff6e66e73f7b03a6900e3b9611e0aba48bebd5b3
Van Thuan <thuannv06022001@gmail.com>
2025-09-25 13:44:56 +0700

    hmm

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -3,7 +3,7 @@ import { Webhook } from "svix";
 
 const clerkWebhooks = async (req, res) => {
     try {
-        const whookhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
         const headers = {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
