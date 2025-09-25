b900e71ee3acccea8dcc5b6d69502cc5fe125509
Van Thuan <thuannv06022001@gmail.com>
2025-09-25 10:04:12 +0700

    update api

--- server/models/User.js
+++ server/models/User.js
@@ -10,6 +10,6 @@ const userSchema = mongoose.Schema({
 
 }, { timestamps: true });
 
-const User = mongoose.model.apply("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
