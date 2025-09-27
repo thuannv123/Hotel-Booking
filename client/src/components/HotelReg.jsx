30a8c289173ee978467a53abe464c6f6e81b2337
Van Thuan <thuannv06022001@gmail.com>
2025-09-27 09:19:43 +0700

    update hotel

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -30,7 +30,7 @@ const HotelReg = () => {
 
     return (
         <div onClick={() => setShowHotelReg(false)} className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
-            <form onSubmit={onSubmitHandler} onClick={(e) => e.stopPropagation} className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
+            <form onSubmit={onSubmitHandler} onClick={(e) => e.stopPropagation()} className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
                 <img src={assets.regImage} alt="reg-image" className='w-1/2 rounded-xl hidden md:block' />
                 <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
                     <img src={assets.closeIcon} alt="close-icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' onClick={() => setShowHotelReg(false)} />
